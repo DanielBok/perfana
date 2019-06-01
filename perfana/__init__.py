16f1bd6b2a17d638e12a60daba39979a0f3af073
Daniel Bok <daniel.bok@outlook.com>
2019-06-01 12:59:26 +0800

    Version and setup for package building

--- perfana/__init__.py
+++ perfana/__init__.py
@@ -1,1 +1,7 @@
 from perfana.pd_ext import PpaSeriesAccessor, PpaFrameAccessor
+from ._version import get_versions
+
+v = get_versions()
+__version__ = v.get('closest-tag', v['version'])
+
+del v, get_versions
---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import find_packages, setup
+
+import versioneer
+
+PACKAGE_NAME = 'perfana'
+
+cmdclass = versioneer.get_cmdclass()
+
+install_requires = [
+    'copulae >=0.3',
+    'numpy',
+    'nlopt >=2.6',
+    'scipy >=1.1',
+    'pandas >=0.23'
+]
+
+setup(
+    name=PACKAGE_NAME,
+    license='MIT',
+    version=versioneer.get_version(),
+    description='Toolbox for performance and portfolio analytics',
+    author='Daniel Bok',
+    author_email='daniel.bok@outlook.com',
+    packages=find_packages(include=['perfana', 'perfana.*']),
+    url='https://github.com/DanielBok/perfana',
+    cmdclass=cmdclass,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    install_requires=install_requires,
+    python_requires='>=3.6',
+    zip_safe=False
+)
