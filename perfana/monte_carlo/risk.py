Bug fix in tail loss calculation

perfana/monte_carlo/risk.py
@@ -638,7 +638,7 @@ def tail_loss(data: np.ndarray,
 
     mask = port <= threshold
     prob = mask.mean()
-    exp = port[mask].mean()
+    exp = (mask * port).mean()
 
     return TailLoss(prob, exp)
 